fd816249115034c73612f1f32fdb042aeab2188d
ShimomiyaYuuki <yuukiud@gmail.com>
2025-02-24 21:10:07 +0900

    セクション2完了

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//セクション2のコードを纏めたもの
 const userName = 'Namae';
 string = `Hello ${userName}`;
 console.log(string);
@@ -71,4 +71,62 @@ console.log(string + `改行
      typeof [1,2,4]
      typeof null
      typeof undefined
-     
+
+    //26. 関数を使い、何度も呼び出すことができるコードのかたまりを作る方法
+     function add() {
+        console.log(1 + 1);
+     }
+     add();
+
+    //27.「パラメーター」と「引数」
+    function add(num1, num2) {
+        return console.log(num1 + num2);
+        console.log('ここはreturnの後なので、無視される');
+        
+    }
+    function add(num1, num2) {
+        return console.log(num1 + num2);
+        console.log('ここはreturnの後なので、無視される');
+        
+    }
+    add(1, 6);
+    add(2, 7);
+    add(3, 8);
+
+    //28.return文を使って、値を返す関数を作成する方法
+    function add(num1, num2) {
+        console.log('ここはreturnの後なので、無視される');
+    }
+    const returnedValue = add(1, 6);
+    console.log(returnedValue); //undefinedが表示される
+
+    //29. 関数の位置は先頭に巻き上げられる
+    const returnedValue2 = add(1, 6);
+    console.log(returnedValue2); //7が表示される。関数の宣言はコードの先頭に巻き上げられるため、関数を呼び出す前に関数を宣言してもエラーが発生しない。これを「巻き上げ」と呼ぶ。
+    function add(num1, num2) { //
+        return num1 + num2;
+    }
+
+    //30. 変数を参照できる範囲であるスコープはこうなっている
+    const newValue = 'addから参照できる変数';
+    function add(num1, num2) {
+        console.log(newValue); //addから参照できる変数が表示される
+        const value = num1 + num2;
+        console.log(value); 
+        return value;
+    }
+    console.log(value); //エラーが発生する。関数内で宣言された変数は関数内でしか参照できない。これを「スコープ」と呼ぶ。
+    
+    //グローバル変数とローカル変数で同じ変数名があるときはどちらを参照するか
+    const Value = 'A';
+    function add(num1, num2) {
+        const value = num1 + num2;
+        console.log(value); // 3が表示される
+        return value;
+    }
+
+    
+    //31. JavaScriptでコメントを書く２つの方法
+    /* 
+    複数行のコメント
+    */
