365698bf19dcf2f016491ed24b35cee661123e9e
ShimomiyaYuuki <yuukiud@gmail.com>
2025-02-24 18:17:51 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+
+const userName = 'Namae';
+string = `Hello ${userName}`;
+console.log(string);
+console.log(string + '\nYou will die');
+console.log(string + `改行
+    できるよ`);
+
+    const userInput = '10.1';
+    let calcResult;
+    calcResult = Number(userInput) + 10;
+    console.log(calcResult);
+    calcResult = parseInt(userInput) + 10;
+    console.log(calcResult);
+    calcResult = parseFloat(userInput) + 10;
+    console.log(calcResult);
+    calcResult = +userInput + 10;
+    console.log(calcResult);
+
+
+    const tenNumber = 10;
+    calcResult = '1' + String(tenNumber);
+    console.log(calcResult);
+    calcResult = '1' + tenNumber.toString();
+    console.log(calcResult);
+
+    let boolean = true;
+    boolean = false;
+
+     
